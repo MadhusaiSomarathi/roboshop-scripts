4d6e976a6b6c1dbf6d3064d8ded5a62f34e18232
madhusai_24 <somarathi.sai@opstree.com>
2023-01-30 15:51:02 +0530

    Added frontend file

---
+++ frontend.sh
@@ -0,0 +1,28 @@
+LOG_FILE=/tmp/frontend
+
+source common.sh
+
+echo Installing Nginx
+yum install nginx -y &>>$LOG_FILE
+StatusCheck $?
+
+echo Downloading Nginx Web Content
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+StatusCheck $?
+
+cd /usr/share/nginx/html
+
+echo Removing Old Web Content
+rm -rf * &>>/tmp/frontend
+StatusCheck $?
+
+echo Extracting Web Content
+unzip /tmp/frontend.zip &>>$LOG_FILE
+StatusCheck $?
+
+
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
+
+
+
