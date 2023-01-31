9c95e210845bf135e5e03d1ca25fc75352191915
madhusai_24 <somarathi.sai@opstree.com>
2023-01-31 09:49:08 +0530

    Added dispatch file

---
+++ dispatch.sh
@@ -0,0 +1,24 @@
+COMPONENT=dispatch
+source common.sh
+echo Intsall GoLang
+yum install golang -y
+
+echo AddUser
+useradd roboshop
+sudo su roboshop
+
+curl -L -s -o /tmp/${COMPONENT}.zip https://github.com/roboshop-devops-project/${COMPONENT}/archive/refs/heads/main.zip
+$ unzip /tmp/${COMPONENT}.zip
+unzip /tmp/${COMPONENT}.zip
+mv ${COMPONENT}-main ${COMPONENT}
+cd ${COMPONENT}
+
+
+go mod init ${COMPONENT}
+go get
+go build
+
+mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+systemctl daemon-reload
+systemctl enable ${COMPONENT}
+systemctl start ${COMPONENT}
