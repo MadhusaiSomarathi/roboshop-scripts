1d2087c24518229be92e9ade93dadc604224a141
madhusai_24 <somarathi.sai@opstree.com>
2023-01-31 10:22:48 +0530

    Added dispatch file changes

--- dispatch.sh
+++ dispatch.sh
@@ -8,17 +8,15 @@ useradd roboshop
 sudo su roboshop
 
 curl -L -s -o /tmp/${COMPONENT}.zip https://github.com/roboshop-devops-project/${COMPONENT}/archive/refs/heads/main.zip
-$ unzip /tmp/${COMPONENT}.zip
 unzip /tmp/${COMPONENT}.zip
 mv ${COMPONENT}-main ${COMPONENT}
 cd ${COMPONENT}
-
 
 go mod init ${COMPONENT}
 go get
 go build
 
-mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-systemctl daemon-reload
-systemctl enable ${COMPONENT}
-systemctl start ${COMPONENT}
+sudo mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+sudo systemctl daemon-reload
+sudo systemctl enable ${COMPONENT}
+sudo systemctl start ${COMPONENT}
