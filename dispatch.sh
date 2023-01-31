2c5c71acad6047cdd49c64dffa35f97799f5e4e7
madhusai_24 <somarathi.sai@opstree.com>
2023-01-31 10:28:27 +0530

    Added dispatch file changes again

--- dispatch.sh
+++ dispatch.sh
@@ -1,22 +1,13 @@
 COMPONENT=dispatch
+LOG_FILE=/tmp/${COMPONENT}
 source common.sh
 echo Intsall GoLang
 yum install golang -y
 
-echo AddUser
-useradd roboshop
-sudo su roboshop
-
-curl -L -s -o /tmp/${COMPONENT}.zip https://github.com/roboshop-devops-project/${COMPONENT}/archive/refs/heads/main.zip
-unzip /tmp/${COMPONENT}.zip
-mv ${COMPONENT}-main ${COMPONENT}
-cd ${COMPONENT}
+APP_PREREQ
 
 go mod init ${COMPONENT}
 go get
 go build
 
-sudo mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-sudo systemctl daemon-reload
-sudo systemctl enable ${COMPONENT}
-sudo systemctl start ${COMPONENT}
+SYSTEMD_SETUP
